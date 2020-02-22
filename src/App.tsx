bee0bfa6ce9433eaf770c7826adb127b400153b6
Vladzavr <vlad8zavr@gmail.com>
2020-02-22 20:14:56 +0300

    lil update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import Test from './Test';
 
+interface Child {
+    text: string;
+};
+
+interface State {
+    type: string;
+    children: Child[];
+};
+
 const App = () => (
     <div className="wrapper">
         <div className="item"></div>
